e20cfb5e84af90574f00e3da3bb2f6f091b93801
Jaagup Kippar <jaagup@tlu.ee>
2021-03-09 11:10:37 +0200

    liides

--- 0309_liidesed/nimeuuringud/Proov1.java
+++ 0309_liidesed/nimeuuringud/Proov1.java
@@ -1,6 +1,8 @@
 public class Proov1{
 	public static void main(String[] arg){
 		TekstistEraldaja te=new SonaEraldaja(1);
+		TekstistEraldaja se=new SoliidneEraldaja();
 		System.out.println(te.eralda("Juku Juurikas"));
+		System.out.println(se.eralda("Juku Juurikas"));
 	}
 }
---
+++ 0309_liidesed/nimeuuringud/SoliidneEraldaja.java
@@ -0,0 +1,8 @@
+public class SoliidneEraldaja implements TekstistEraldaja{
+	TekstistEraldaja esitaheEraldaja=new EsitaheEraldaja();
+	TekstistEraldaja teiseSonaEraldaja=new SonaEraldaja(1);
+	public String eralda(String s){
+		return esitaheEraldaja.eralda(s)+". "+
+		       teiseSonaEraldaja.eralda(s);
+	}
+}
