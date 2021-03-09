9e747e978891b5914596e67b91ca87223baa58cb
Jaagup Kippar <jaagup@tlu.ee>
2021-03-09 09:27:02 +0200

    liides2

--- 0309_liidesed/Proov1.java
+++ 0309_liidesed/Proov1.java
@@ -1,9 +1,17 @@
 public class Proov1{
 	public static void main(String[] arg){
-		Hoidla h=new LihtneHoidla();
+//		Hoidla h=new LihtneHoidla();
+		Hoidla h=new TalletavHoidla(5);
 		for(int i=0; i<arg.length; i++){
 			h.lisa(Integer.parseInt(arg[i]));
 		}
 		System.out.println(h.kysiSumma());
+		if(h instanceof TalletavHoidla){
+			TalletavHoidla th=(TalletavHoidla)h;
+			if(th.kysiArvud().length>0){
+				System.out.println(
+				 "esimene arv oli "+th.kysiArvud()[0]);
+			}
+		}
 	}
 }
