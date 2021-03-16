26a06aec63c7871ab72a659d7fea79c60f0e2513
Jaagup Kippar <jaagup@tlu.ee>
2021-03-16 10:30:17 +0200

    testkalkulaator

---
+++ 0316_testid/KalkulaatoriTestid.java
@@ -0,0 +1,15 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+public class KalkulaatoriTestid{
+	KalkulaatoriLiides k=null;
+	@Before
+	public void puhasta(){
+		k.vajutus("C");
+	}
+	@Test
+	public void sisestus1(){
+		assertEquals("0", k.kuvatav());
+		k.vajutus("2");
+		assertEquals("2", k.kuvatav());
+	}
+}
